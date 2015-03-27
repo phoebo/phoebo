ea8f041ec1e18599996224d13891fe808b06a9b5
Adam Staněk <adam.stanek@v3net.cz>
2015-03-27 14:53:25 +0100

    Fixed Redis.composite_key

--- lib/core_ext/redis.rb
+++ lib/core_ext/redis.rb
@@ -1,7 +1,7 @@
 class Redis
   def self.composite_key(*args)
     args.collect do |arg|
-      arg.gsub(/\\/, '\\\\\\\\').gsub(/\//, '\\\\/')
+      arg.to_s.gsub(/\\/, '\\\\\\\\').gsub(/\//, '\\\\/')
     end.join('/')
   end
 end
