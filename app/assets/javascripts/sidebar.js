6bef4af5a9e75d12b2ff1ac2016036053496b8b5
Adam Staněk <adam.stanek@v3net.cz>
2015-04-03 00:38:45 +0200

    Improved sidebar

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,20 +1,21 @@
-$(document).ready(function () {
-	$(document).on("click", '.toggle-nav-collapse', function(e) {
-		var collapsed, expanded;
-		e.preventDefault();
-		collapsed = 'page-sidebar-collapsed';
-		expanded = 'page-sidebar-expanded';
+$(document).tooltip({
+	selector: '.page-sidebar-collapsed .nav-sidebar a',
+	placement: 'right'
+});
 
-		if ($('.page-with-sidebar').hasClass(collapsed)) {
-			$('.page-with-sidebar').removeClass(collapsed).addClass(expanded);
-			$('.toggle-nav-collapse i').removeClass('fa-angle-right').addClass('fa-angle-left');
-		} else {
-			$('.page-with-sidebar').removeClass(expanded).addClass(collapsed);
-			$('.toggle-nav-collapse i').removeClass('fa-angle-left').addClass('fa-angle-right');
-		}
-	});
+$(document).on("click", '.toggle-nav-collapse', function(e) {
+	var collapsed, expanded;
+	e.preventDefault();
+	collapsed = 'page-sidebar-collapsed';
+	expanded = 'page-sidebar-expanded';
 
-	$('.has_bottom_tooltip').each(function () {
-		$(this).tooltip();
-	});
+	if ($('.page-with-sidebar').hasClass(collapsed)) {
+		$('.page-with-sidebar').removeClass(collapsed).addClass(expanded);
+		$('.toggle-nav-collapse i').removeClass('fa-angle-right').addClass('fa-angle-left');
+	} else {
+		$('.page-with-sidebar').removeClass(expanded).addClass(collapsed);
+		$('.toggle-nav-collapse i').removeClass('fa-angle-left').addClass('fa-angle-right');
+	}
+
+	$.cookie("collapsed_nav", $('.page-with-sidebar').hasClass(collapsed), { path: '/' });
 });
